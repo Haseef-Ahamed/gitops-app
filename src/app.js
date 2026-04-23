3798d34127c5228fed36bea08208886c1b9177a2
Haseef-Ahamed <mshaseefat@gmail.com>
2026-04-23 17:53:57 +0530

    update the version on Dev Env

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const VERSION = process.env.APP_VERSION || 'v3.0.0';
+const VERSION = process.env.APP_VERSION || 'v4.0.0';
 
 // Health check endpoint
 app.get('/health', (req, res) => {
